935738dc031fa3044395e6929cf9d4e0dc37c224
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2016-02-11 15:49:35 +0100

    Fixed methbod for IDL clean; Improved pattern match for lng search

--- unidevtools.py
+++ unidevtools.py
@@ -8,17 +8,21 @@ class CleanIdlCommand(sublime_plugin.TextCommand):
 		afterCode = [self.view.substr(x) for x in regions if x.a > source.b]
 		unusedIDL = []
 		for idl in beforeCode:
-			if not self.view.substr(idl) in afterCode:
+			if self.view.substr(idl) not in afterCode:
 				unusedIDL.append(idl)
-		for idl in unusedIDL:
-			line = self.view.full_line(idl)
-			self.view.erase(edit, line)
+		#f = sublime.active_window().new_file()
+		for line in reversed(unusedIDL):
+			self.view.erase(edit, self.view.full_line(line))
+			#counter += f.insert(edit, counter, self.view.substr(self.view.full_line(line)))
+		#f.set_syntax_file("Packages/User/BETA.sublime-syntax")
+		#sublime.active_window().focus_view(f)
 
 class ListAllStringsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		regions = self.view.find_all("('|\").+('|\")")
 		sourceStart = self.view.find(">SOURCE", 0)
-		regions = [x for x in regions if x.b > sourceStart.b and "lng_text" not in self.view.substr(self.view.full_line(x))]
+		pattern = re.compile(".+(?i)lng_text.+")
+		regions = [x for x in regions if x.b > sourceStart.b and not pattern.match(self.view.substr(self.view.full_line(x)))] #"lng_text" not in self.view.substr(self.view.full_line(x))]
 		f = sublime.active_window().new_file()
 		counter = 0
 		for line in regions:
